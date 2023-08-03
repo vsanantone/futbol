f04d877996aaca12225bd7e5ee8f6c0030aaf163
Nick Spencer <strangebrew202@gmail.com>
2023-08-03 11:48:52 -0600

    Add: finish logic for #average_goals_per_game and #average_goals_by_season

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -73,8 +73,28 @@ class GameStats
   end
 
   def average_goals_per_game
+    goals = @games.sum do |game|
+      game.home_goals + game.away_goals
+    end
+    (goals.to_f / games.count.to_f).round(2)
   end
     
   def average_goals_by_season
+    games_per_season = Hash.new(0)
+    @games.each do |game|
+      games_per_season[game.season] += 1
+    end
+   goals_per_season = Hash.new(0)
+   @games.each do |game|
+    goals_per_season[game.season] += (game.away_goals + game.home_goals)
+   end
+   {
+    "20122013" => (goals_per_season["20122013"].to_f / games_per_season["20122013"].to_f).round(2),
+    "20132014" => (goals_per_season["20132014"].to_f / games_per_season["20132014"].to_f).round(2),
+    "20142015" => (goals_per_season["20142015"].to_f / games_per_season["20142015"].to_f).round(2),
+    "20152016" => (goals_per_season["20152016"].to_f / games_per_season["20152016"].to_f).round(2),
+    "20162017" => (goals_per_season["20162017"].to_f / games_per_season["20162017"].to_f).round(2),
+    "20172018" => (goals_per_season["20172018"].to_f / games_per_season["20172018"].to_f).round(2) 
+   }
   end
 end
