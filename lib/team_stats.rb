07412616bd70c964bd67b163412ed4e907263dd7
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-08-03 11:17:29 -0700

    Flush out some of the kinks in the team_stats method and rewrite some of the spec too

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -4,20 +4,23 @@ require "csv"
 class Team_Stats
   attr_reader :teams_data, :game_teams_data
 
-  def initialize(teams_data, game_teams_data)
-    @teams_data = teams_data
-    @game_teams_data = game_teams_data
+  def initialize(data_hash)
+    @teams_data = load_csv(data_hash[:teams])
+    @game_teams_data = load_csv(data_hash[:game_teams])
   end
 
   def get_team_stats(team_id)
     team_stats = {
+      team_name: nil,
       wins: 0,
       losses: 0,
       shots_on_goal: 0,
       total_goals: 0
     }
 
-    relevant_game_teams = game_teams_data.select { |game_team| game_team[:team_id] == team_id }
+    team = find_by_id(team_id)
+
+    relevant_game_teams = @game_teams_data.select { |game_team| game_team[:team_id] == team_id }
 
     relevant_game_teams.each do |game_team|
       if game_team[:result] == 'WIN'
@@ -29,11 +32,17 @@ class Team_Stats
       team_stats[:shots_on_goal] += game_team[:shots].to_i
       team_stats[:total_goals] += game_team[:goals].to_i
     end
-
+    team_stats[:team_name] = team.name
     team_stats
   end
 
   def load_csv(file_path)
     CSV.read(file_path, headers: true, header_converters: :symbol)
+  end
+
+  def find_by_id(team_id)
+    team_data = @teams_data.find do |team|
+      team[:team_id] == team_id
+    end
   end
 end
--- spec/team_stats_spec.rb
+++ spec/team_stats_spec.rb
@@ -16,11 +16,21 @@ RSpec.describe Team_Stats do
 
   describe "#initialize" do
   it "exists" do
-    expect(team_stats).to be_a(Team_Stats)
+    expect(team_stats).to be_an_instance_of(Team_Stats)
   end
 
   it "reads in CSVs and creates team stats objects" do
     expect(team_stats.teams_data).to all be_a(CSV::Row)
     expect(team_stats.game_teams_data).to all be_a(CSV::Row)
   end
+
+  describe "#get_team_stats" do
+    it "returns team statistics based on the team_id" do
+      team_id = 4
+      stats = team_stats.get_team_stats(team_id)
+
+      expect(stats[:wins]).to eq(1)
+    end
+  end
+end
 end
