1f27ad9b52620cb111c60d9581c2ce1eb43f4aea
Nick Spencer <strangebrew202@gmail.com>
2023-08-04 12:29:16 -0600

    Refactor: remove commented code that was moved into Stats class

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -13,33 +13,6 @@ class LeagueStats < Stats
     @game_teams = build_game_teams(data)
   end
 
-  # def build_games(data)
-  #   games = []
-  #   info = CSV.parse (File.read(data[:games])), headers: true, header_converters: :symbol
-  #   info.each do |row|
-  #     games << Game.new(row)
-  #   end
-  #   games
-  # end
-
-  # def build_teams(data)
-  #   teams = []
-  #   team_info = CSV.parse (File.read(data[:teams])), headers: true, header_converters: :symbol
-  #   team_info.each do |row|
-  #     teams << Team.new(row)
-  #   end
-  #   teams
-  # end
-
-  # def build_game_teams(data)
-  #   game_teams = []
-  #   game_teams_info = CSV.parse (File.read(data[:game_teams])), headers: true, header_converters: :symbol
-  #   game_teams_info.each do |row|
-  #     game_teams << GameTeam.new(row)
-  #   end
-  #   game_teams
-  # end
-
   def count_of_teams
     @teams.count
   end
