9f5b5c3c0f32f22993bdb1ea0616f43a8c32677b
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-08-03 22:54:36 -0700

    Debugging the team_stats file

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -2,11 +2,21 @@ require "./lib/team.rb"
 require "csv"
 
 class Team_Stats
-  attr_reader :teams_data, :game_teams_data
+  attr_reader :teams,
+              :teams_data,
+              :game_teams_data
 
-  def initialize(data_hash)
-    @teams_data = load_csv(data_hash[:teams])
-    @game_teams_data = load_csv(data_hash[:game_teams])
+  def initialize(data)
+    @teams_data = CSV.parse(File.read(data[:teams]), headers: true, header_converters: :symbol)
+    @game_teams_data = CSV.parse(File.read(data[:game_teams]), headers: true, header_converters: :symbol)
+    @teams = []
+    build_teams(@teams_data)
+  end
+
+  def build_teams(data)
+    data.each do |row|
+      @teams << Team.new(id: row[:team_id], name: row[:teamName].to_s)
+    end
   end
 
   def get_team_stats(team_id)
@@ -19,8 +29,11 @@ class Team_Stats
     }
 
     team = find_by_id(team_id)
+    team_stats[:team_name] = team[:name]
 
-    relevant_game_teams = @game_teams_data.select { |game_team| game_team[:team_id] == team_id }
+    relevant_game_teams = @game_teams_data.select do |game_team| 
+      game_team[:team_id] == team_id.to_s
+    end
 
     relevant_game_teams.each do |game_team|
       if game_team[:result] == 'WIN'
@@ -28,21 +41,24 @@ class Team_Stats
       elsif game_team[:result] == 'LOSS'
         team_stats[:losses] += 1
       end
-
       team_stats[:shots_on_goal] += game_team[:shots].to_i
       team_stats[:total_goals] += game_team[:goals].to_i
     end
-    team_stats[:team_name] = team.name
     team_stats
   end
 
-  def load_csv(file_path)
-    CSV.read(file_path, headers: true, header_converters: :symbol)
-  end
+  # iterate through my teams array
 
   def find_by_id(team_id)
-    team_data = @teams_data.find do |team|
-      team[:team_id] == team_id
+    # require 'pry';binding.pry
+    searched_team = @teams.find(team_id) do |team|
+      team.id == team_id
     end
+    searched_team
   end
+  #   team_data = @teams_data.find do |team|
+  #     team[:team_id] == team_id
+  #   end
+  #   Team.new(team_id: team_data[:team_id], name: team_data[:teamName])
+  # end
 end
--- spec/team_stats_spec.rb
+++ spec/team_stats_spec.rb
@@ -15,22 +15,38 @@ RSpec.describe Team_Stats do
   let(:team_stats) { Team_Stats.new(data_hash) }
 
   describe "#initialize" do
-  it "exists" do
-    expect(team_stats).to be_an_instance_of(Team_Stats)
-  end
+    it "exists" do
+      expect(team_stats).to be_an_instance_of(Team_Stats)
+    end
 
-  it "reads in CSVs and creates team stats objects" do
-    expect(team_stats.teams_data).to all be_a(CSV::Row)
-    expect(team_stats.game_teams_data).to all be_a(CSV::Row)
+    it "reads in CSVs and creates team stats objects" do
+      expect(team_stats.teams_data).to all be_a(CSV::Row)
+      expect(team_stats.game_teams_data).to all be_a(CSV::Row)
+    end
   end
 
   describe "#get_team_stats" do
     it "returns team statistics based on the team_id" do
-      team_id = 4
+      team1 = team_stats.teams.sample
+      team_id = team1.id
       stats = team_stats.get_team_stats(team_id)
 
+      expect(stats[:team_name]).to eq("Chicago Fire")
       expect(stats[:wins]).to eq(1)
+      # expect(stats[:wins]).to eq(1)
+      # expect(stats[:wins]).to eq(1)
+      # expect(stats[:wins]).to eq(1)
     end
   end
-end
+
+  describe "#find_by_id" do
+    it "returns a Team object based on the team_id" do
+      team1 = team_stats.teams.sample
+      team_id = team1.id
+      team = team_stats.find_by_id(team_id)
+
+      expect(team).to be_an_instance_of(Team)
+      
+    end
+  end
 end
