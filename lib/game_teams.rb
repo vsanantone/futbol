12f7c34425dd4afcd6b242e9139542f7105e5d14
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-06 17:27:03 -0600

    update: Add game_id attribute and @hoa attribute to game_teams class

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -1,12 +1,14 @@
 class GameTeam
-  attr_reader :team_id, :result, :head_coach, :shots, :goals, :tackles
+  attr_reader :game_id, :team_id, :result, :head_coach, :shots, :goals, :tackles
 
   def initialize(game_team_data)
+    @game_id = game_team_data[:game_id]
     @team_id = game_team_data[:team_id]
     @result = game_team_data[:result]
     @head_coach = game_team_data[:head_coach]
     @shots = game_team_data[:shots]
     @goals = game_team_data[:goals]
     @tackles = game_team_data[:tackles]
+    @hoa = game_team_data[:hoa]
   end
 end
