6906397b82793cd383fd68c068791a27736fc281
Nick Spencer <strangebrew202@gmail.com>
2023-08-06 22:25:36 -0600

    Add #lowest_scoring_visitor, #lowest_socring_home_team, #team_info. Also adjust output of #team_info to match format required by spec harness, and adjusted our own spec file for league info to match new format

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -86,4 +86,24 @@ RSpec.describe StatTracker do
   it "#hightest_scoring_home_team" do
     expect(@stat_tracker.highest_scoring_home_team).to eq "Reign FC"
   end
+
+  it "#lowest_scoring_visitor" do
+    expect(@stat_tracker.lowest_scoring_visitor).to eq "San Jose Earthquakes"
+  end
+
+  it "#lowest_scoring_home_team" do
+    expect(@stat_tracker.lowest_scoring_home_team).to eq "Utah Royals FC"
+  end
+
+  it "#team_info" do
+  expected = {
+    "team_id" => "18",
+    "franchise_id" => "34",
+    "team_name" => "Minnesota United FC",
+    "abbreviation" => "MIN",
+    "link" => "/api/v1/teams/18"
+  }
+
+  expect(@stat_tracker.team_info("18")).to eq expected
+  end
 end
