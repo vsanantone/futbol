310f3c5f670a03543be581e3ab4db23aee628e61
Victor Sanchez <vsanantone@gmail.com>
2023-08-06 19:30:09 -0500

    add lowest_scoring_home_team method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -73,4 +73,8 @@ class StatTracker
   def lowest_scoring_visitor
     @league_stats.lowest_scoring_visitor 
   end
+
+  def lowest_scoring_home_team
+    @league_stats.lowest_scoring_home_team 
+  end
 end
