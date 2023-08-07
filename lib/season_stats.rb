e7dbb0c839719d6c0aa94582ac7287e2e25faa9c
Victor Sanchez <vsanantone@gmail.com>
2023-08-07 10:05:13 -0500

    feat: delete duplicate build methods

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -8,34 +8,6 @@ class SeasonStats
     @game_teams = build_game_teams(data)  
   end
 
-  def build_games(data)
-    games = []
-    info = CSV.parse (File.read(data[:games])), headers: true, header_converters: :symbol
-    info.each do |row|
-      games << Game.new(row)
-    end
-    games
-  end
-
-  def build_teams(data)
-    teams = []
-    team_info = CSV.parse (File.read(data[:teams])), headers: true, header_converters: :symbol
-    team_info.each do |row|
-      teams << Team.new(row)
-    end
-    teams
-  end
-
-  
-  def build_game_teams(data)
-    game_teams = []
-    game_teams_info = CSV.parse (File.read(data[:game_teams])), headers: true, header_converters: :symbol
-    game_teams_info.each do |row|
-      game_teams << GameTeam.new(row)
-    end
-    game_teams
-  end
-  
   def indiv_season(season_id)
     season = games.find_all do |game|
       season_id == game.season 
@@ -82,6 +54,14 @@ class SeasonStats
     coach_win_percentages[0]
   end
 
+  def most_tackles(season_id)
+    season = indiv_season(season_id)
+    most_tackles = season.max_by do |game|
+      game.tackles
+      require 'pry'; binding.pry
+    end
+  end
+
   # def worst_coach(season_id)
   #   season = win_or_loss_per_season(season_id, "WIN")
   #   most_wins = season.map do |game|
@@ -99,3 +79,5 @@ class SeasonStats
   # end
     
 end
+
+
